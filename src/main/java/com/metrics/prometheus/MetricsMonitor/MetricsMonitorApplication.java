4d577fe8f4c787b6370e502a492e7574c9bf5e5d
Shubham Goel <shubhamgoel@Shubhams-MacBook-Air.local>
2022-10-21 10:10:55 +0530

    first commit

---
+++ src/main/java/com/metrics/prometheus/MetricsMonitor/MetricsMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.metrics.prometheus.MetricsMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricsMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metrics/prometheus/MetricsMonitor/MetricsMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metrics.prometheus.MetricsMonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricsMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
